2e6d08a05bc89d21947eb509aab5c09636201503
Jake Spencer <jake@uncarbonatedsoftware.com>
2015-04-02 21:40:51 -0500

    Got chat messages in response to commands working

--- src/EzProtect/Main.php
+++ src/EzProtect/Main.php
@@ -5,6 +5,8 @@ namespace EzProtect;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\network\protocol\ChatPacket;
+use pocketmine\Player;
 
 class Main extends PluginBase{
 
@@ -23,7 +25,19 @@ class Main extends PluginBase{
         public function onCommand(CommandSender $sender, Command $command, $label, array $args){
                 if(strtolower($command->getName()) === "ezprotect"){
                 // Execute logic
-                $this->getLogger()->info("You called the EzProtect command! Good work!");
+                // $this->getLogger()->info("You called the EzProtect command! Good work!");
+
+                if($sender instanceof Player){
+                  $senderName = $sender->getName();
+                  // $pk = new ChatPacket();
+                  // $pk->message = "You called the EzProtect command! Good work, " . $senderName . "!";
+                  // $sender->dataPacket($pk);
+                  $sender->sendMessage("You called the EzProtect command!!! Good work, " . $senderName . "!");
+                }
+                else{
+                  $this->getLogger()->info("Not a player called the EzProtect command.");
+                }
+
                 return true;
         }
 
